7315f2b72132ada02aba63221d80d7e3616ebfe9
leon-kn <leontiy.knyazev@gmail.com>
2023-04-11 16:29:13 +0500

    feat: initialize state

---
+++ src/store/reducers/CharacterSlice.ts
@@ -0,0 +1,5 @@
+interface CharacterState {}
+
+const initialState = {
+  characters: [],
+};
---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = typeof store;
+export type AppDispatch = typeof store.dispatch;
